d4e7eced383b6591dcdbf85f68fea65eaecd9bff
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-06-16 12:57:38 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo off
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/yourusername/yourrepo/master/urls.txt' -OutFile '%TEMP%\urls.txt'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rk2994329/datas/main/scroll.vbs' -OutFile '%TEMP%\scroll.vbs'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rk2994329/datas/main/ruls.txt' -OutFile '%TEMP%\urls.txt'"
 for /f "tokens=*" %%A in (%TEMP%\urls.txt) do (
     start chrome "%%A"
     timeout /t 5
