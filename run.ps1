fcf322b1f32bfbba63c186702a3049729ddb7967
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-06-17 20:16:31 +0530

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -10,13 +10,13 @@ $urls = Get-Content "$env:TEMP\urls.txt"
 # Loop through each URL
 foreach ($url in $urls) {
     # Open Chrome in a new window and incognito mode
-    Start-Process "chrome.exe" "--new-window --incognito $url"
+    Start-Process "chrome.exe" "--incognito $url"
 
     # Wait for 5 seconds
     Start-Sleep -Seconds 5
 
     # Scroll down 10 times
-    for ($i = 0; $i -lt 10; $i++) {
+    for ($i = 0; $i -lt 150; $i++) {
         [System.Windows.Forms.SendKeys]::SendWait('{DOWN}')
         Start-Sleep -Seconds 1
     }
