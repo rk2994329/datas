94a4e5ae7efeffbbd2f28d10a467728b8984d4a8
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-06-17 19:59:54 +0530

    Delete scroll.ps1

--- scroll.ps1
+++
@@ -1,4 +0,0 @@
-$browser = New-Object -ComObject "InternetExplorer.Application"
-$browser.Visible = $true
-$browser.Navigate("about:blank")
-$browser.Document.parentWindow.execScript("function scrollPage() { window.scrollBy(0, 100); if ((window.innerHeight + window.scrollY) < document.body.offsetHeight) { setTimeout(scrollPage, 200); } } scrollPage();", "JavaScript")
