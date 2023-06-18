22e2b512d17bffa821b3f59449a74d6426470147
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-06-18 18:48:19 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@ powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/r
 for /f "tokens=*" %%A in (%TEMP%\urls.txt) do (
     start msedge.exe --incognito "%%A"
     timeout /t 5
-    powershell -Command "$wshell = New-Object -ComObject WScript.Shell; for ($i = 0; $i -lt 10; $i++){$wshell.SendKeys('{DOWN}');Start-Sleep -Seconds 1;} for ($j = 0; $j -lt 5; $j++){$wshell.SendKeys('{PGDN}');Start-Sleep -Seconds 2;} $wshell.SendKeys('{END}');Start-Sleep -Seconds 5"
+    powershell -Command "$wshell = New-Object -ComObject WScript.Shell; for ($i = 0; $i -lt 150; $i++){$wshell.SendKeys('{DOWN}');Start-Sleep -Seconds 1;} for ($j = 0; $j -lt 5; $j++){$wshell.SendKeys('{PGDN}');Start-Sleep -Seconds 2;} $wshell.SendKeys('{END}');Start-Sleep -Seconds 5"
 )
