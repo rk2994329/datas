19499f87fd27aa096f68f1335af2e60c1fbb34b9
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-06-18 18:44:36 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rk2994329/datas/main/urls.txt' -OutFile '%TEMP%\urls.txt'"
 for /f "tokens=*" %%A in (%TEMP%\urls.txt) do (
-    start chrome --incognito "%%A"
+    start msedge.exe --incognito "%%A"
     timeout /t 5
     powershell -Command "$wshell = New-Object -ComObject WScript.Shell; for ($i = 0; $i -lt 10; $i++){$wshell.SendKeys('{DOWN}');Start-Sleep -Seconds 1;} for ($j = 0; $j -lt 5; $j++){$wshell.SendKeys('{PGDN}');Start-Sleep -Seconds 2;} $wshell.SendKeys('{END}');Start-Sleep -Seconds 5"
 )
