ed57188e09ed9e08b2a2ea674c4fb20939771a93
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-06-18 14:11:55 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,7 @@
 @echo off
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rk2994329/datas/main/scroll.vbs' -OutFile '%TEMP%\scroll.vbs'"
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rk2994329/datas/main/ruls.txt' -OutFile '%TEMP%\urls.txt'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rk2994329/datas/main/urls.txt' -OutFile '%TEMP%\urls.txt'"
 for /f "tokens=*" %%A in (%TEMP%\urls.txt) do (
-    start chrome "%%A"
+    start chrome --incognito "%%A"
     timeout /t 5
-    cscript //nologo %TEMP%\scroll.vbs
+    powershell -Command "$wshell = New-Object -ComObject WScript.Shell; for ($i = 0; $i -lt 150; $i++) { $wshell.SendKeys('{DOWN}'); Start-Sleep -Seconds 1; }"
 )
