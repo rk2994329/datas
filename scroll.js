d3eaf0e9427ff3e5db04ecd33b2de08e8a70166a
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-06-18 14:11:40 +0530

    Delete scroll.js

--- scroll.js
+++
@@ -1,7 +0,0 @@
-function scrollPage() {
-  window.scrollBy(0, 100);
-  if ((window.innerHeight + window.scrollY) < document.body.offsetHeight) {
-    setTimeout(scrollPage, 200);
-  }
-}
-scrollPage();
