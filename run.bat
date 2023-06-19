436f156b7b5f0ad754c370e9fbd7ddac46b5bb59
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-06-19 20:35:08 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 @echo off
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rk2994329/datas/main/urls.txt' -OutFile '%TEMP%\urls.txt'"
-
-for /f "tokens=*" %%A in (%TEMP%\urls.txt) 
-do (
+for /L %%i in (1,1,20) do (
+  for /f "tokens=*" %%A in (%TEMP%\urls.txt) do (
     start msedge.exe -inprivate --start-maximized "%%A"
     timeout /t 5
     powershell -Command "$wshell = New-Object -ComObject WScript.Shell; $wshell.SendKeys('{END}');Start-Sleep -Seconds 5; $wshell.SendKeys('{HOME}'); Start-Sleep -Seconds 5; for ($i = 0; $i -lt 50; $i++){$wshell.SendKeys('{DOWN}');Start-Sleep -Seconds 3;}"
     taskkill /f /im msedge.exe
+  )
 )
