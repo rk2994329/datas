acd4c33654da13fa874b15c5d3a7619498f606f6
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-06-20 14:57:38 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ wget -O urls.txt https://raw.githubusercontent.com/rk2994329/datas/main/urls.txt
 # Read each URL from urls.txt and open it in Firefox
 while read url; do
   firefox --private-window "$url"
+  firefox_window=$(wmctrl -l | grep "Mozilla Firefox" | awk '{print \$1}')
+  wmctrl -i -a "$firefox_window"
   sleep 5 
   xdotool key End
   sleep 5
