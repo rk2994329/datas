ec1333daee3d927a6d1f8c5353b5f40d5cb73d40
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-07-05 16:10:05 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,9 +4,7 @@ for /L %%i in (1,1,20) do (
   for /f "tokens=*" %%A in (%TEMP%\urls.txt) do (
     start msedge.exe -inprivate --start-maximized "%%A"
     timeout /t 3
-    powershell -Command "$wshell.AppActivate('Microsoft Edge')"
-    timeout /t 5
-    powershell -Command "$wshell = New-Object -ComObject WScript.Shell; $wshell.SendKeys('{END}');Start-Sleep -Seconds 5; $wshell.SendKeys('{HOME}'); Start-Sleep -Seconds 5; for ($i = 0; $i -lt 75; $i++){$wshell.SendKeys('{DOWN}');Start-Sleep -Seconds 4;}"
+    powershell -Command "$wshell = New-Object -ComObject WScript.Shell; $wshell.SendKeys('{END}');Start-Sleep -Seconds 5; $wshell.SendKeys('{HOME}'); Start-Sleep -Seconds 5; for ($i = 0; $i -lt 50; $i++){$wshell.SendKeys('{DOWN}');Start-Sleep -Seconds 6;}"
   )
   taskkill /f /im msedge.exe
     if errorlevel 1 (
