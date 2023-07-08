4fdb07cdfa1bdf52e7059decf80fabcefe598533
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-07-08 12:39:02 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,14 +1,15 @@
 @echo off
+start notepad
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rk2994329/datas/main/urls.txt' -OutFile '%TEMP%\urls.txt'"
 for /L %%i in (1,1,20) do (
   for /f "tokens=*" %%A in (%TEMP%\urls.txt) do (
-    start msedge.exe -inprivate --start-maximized "%%A"
-    timeout /t 3
-    powershell -Command "$wshell = New-Object -ComObject WScript.Shell; $wshell.SendKeys('{END}');Start-Sleep -Seconds 5; $wshell.SendKeys('{HOME}'); Start-Sleep -Seconds 5; for ($i = 0; $i -lt 50; $i++){$wshell.SendKeys('{DOWN}');Start-Sleep -Seconds 6;}"
+    start msedge.exe --start-maximized -inprivate "%%A"
+    timeout /t 5
+    powershell -Command "$wshell = New-Object -ComObject WScript.Shell; $wshell.AppActivate('Microsoft Edge'); $wshell.SendKeys('{END}'); Start-Sleep -Seconds 5; $wshell.SendKeys('{HOME}'); Start-Sleep -Seconds 5; for ($i = 0; $i -lt 60; $i++){$wshell.SendKeys('{DOWN}');Start-Sleep -Seconds 5;}"
   )
   taskkill /f /im msedge.exe
-    if errorlevel 1 (
-    echo edge not found!.
-    ) else ( echo edge closed )
-    timeout /t 5
+  if errorlevel 1 (
+    echo Edge not found!.
+  ) else ( echo Edge closed )
+  timeout /t 5
 )
