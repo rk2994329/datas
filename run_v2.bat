d7c24242a5fb12e6d87e60e289292ee42655da3b
rk2994329 <109289041+rk2994329@users.noreply.github.com>
2023-10-09 12:34:46 +0530

    Create run_v2.bat

---
+++ run_v2.bat
@@ -0,0 +1,15 @@
+@echo off
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rk2994329/datas/main/urls.txt' -OutFile '%TEMP%\urls.txt'"
+set /a num=%random% %% 10 + 15
+for /L %%i in (1,1,%num%) do (
+  for /f "tokens=*" %%A in (%TEMP%\urls.txt) do (
+    start msedge.exe --start-maximized -inprivate "%%A"
+    timeout /t 5
+    powershell -Command "$wshell = New-Object -ComObject WScript.Shell; $wshell.AppActivate('Microsoft Edge'); $wshell.SendKeys('{END}'); Start-Sleep -Seconds 3; $wshell.SendKeys('{HOME}'); Start-Sleep -Seconds 5; for ($i = 0; $i -lt ((Get-Random -Minimum 15 -Maximum 25)); $i++){$wshell.SendKeys('{DOWN}');Start-Sleep -Seconds (Get-Random -Minimum 1 -Maximum 8);}"
+  )
+  taskkill /f /im msedge.exe
+  if errorlevel 1 (
+    echo Edge not found!.
+  ) else ( echo Edge closed )
+  timeout /t 5
+)
